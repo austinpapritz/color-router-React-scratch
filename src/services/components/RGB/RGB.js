156c0305ae6819cd4125b9a1d8ee5261e68ecf96
Austin Papritz <austin@papritz.dev>
2023-01-24 10:59:54 -0800

    changed divs to button in RGB.js, color value logging to console on click

--- src/services/components/RGB/RGB.js
+++ src/services/components/RGB/RGB.js
@@ -5,33 +5,39 @@ import { NavLink, useParams } from 'react-router-dom';
 export default function RGB() {
   const { red, green, blue } = useParams();
 
-  function handleColorChange(value) {}
+  function handleColorChange(color) {
+    console.log('color', color);
+  }
 
   return (
     <div className="alpha-container">
       <div className="rgb-container">
         <NavLink to={'/rgb/244/55/55'}>
-          <div className="red-div" value="red" onClick={(e) => handleColorChange(e.target.value)}>
+          <button
+            className="red-div"
+            value="red"
+            onClick={(e) => handleColorChange(e.target.value)}
+          >
             Red
-          </div>
+          </button>
         </NavLink>
         <NavLink to={'/rgb/244/135/55'}>
-          <div
+          <button
             className="orange-div"
             value="orange"
             onClick={(e) => handleColorChange(e.target.value)}
           >
             Orange
-          </div>
+          </button>
         </NavLink>
         <NavLink to={'/rgb/244/210/55'}>
-          <div
+          <button
             className="yellow-div"
             value="yellow"
             onClick={(e) => handleColorChange(e.target.value)}
           >
             Yellow
-          </div>
+          </button>
         </NavLink>
       </div>
     </div>
